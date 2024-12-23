64cc48015ee5add75fe15cfab913f5f36e1f4559
UsamaBinAbdulWahid <usamaabdulwahid14@gmail.com>
2024-12-23 20:52:57 +0500

    Create a car object

--- Dec-23-2024/Tasks on Objects/script.js
+++ Dec-23-2024/Tasks on Objects/script.js
@@ -8,3 +8,13 @@ console.log("Student Details: ", student);
 //Adding a new property
 student.grade = "A";
 console.log("Updated Student Details: ", student);
+
+//Create a car object
+let car = {
+  make: "Toyota",
+  model: "Corolla",
+  year: 2021,
+  color: "Blue",
+};
+car.color = "Red";
+console.log("Updated car details:", car);
