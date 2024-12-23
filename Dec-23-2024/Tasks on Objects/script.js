732cb6a37182d698df05b0ca58f05b9de27b133a
UsamaBinAbdulWahid <usamaabdulwahid14@gmail.com>
2024-12-23 20:54:09 +0500

    Create multiple objects and store them in an array

--- Dec-23-2024/Tasks on Objects/script.js
+++ Dec-23-2024/Tasks on Objects/script.js
@@ -18,3 +18,11 @@ let car = {
 };
 car.color = "Red";
 console.log("Updated car details:", car);
+
+//Create multiple objects and store them in an array
+let students = [
+  { name: "Usama", age: 22, class: "12th" },
+  { name: "Ali", age: 21, class: "11th" },
+  { name: "Ahamd", age: 23, class: "12th" },
+];
+console.log("Students:", students);
