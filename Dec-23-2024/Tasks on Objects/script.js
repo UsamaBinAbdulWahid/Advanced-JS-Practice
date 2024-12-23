507acd566e7bd1842ce71b9c2205d885cb8dd1ef
UsamaBinAbdulWahid <usamaabdulwahid14@gmail.com>
2024-12-23 20:50:52 +0500

    Create a student object

---
+++ Dec-23-2024/Tasks on Objects/script.js
@@ -0,0 +1,10 @@
+//Create a student object
+let student = {
+  name: "Usama",
+  age: 22,
+  class: "5th",
+};
+console.log("Student Details: ", student);
+//Adding a new property
+student.grade = A;
+console.log("Updated Student Details: ", student);
