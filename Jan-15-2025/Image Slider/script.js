18f0999b42fc86198839d8d4ae7ad93a36665b70
UsamaBinAbdulWahid <usamaabdulwahid14@gmail.com>
2025-01-16 14:19:26 +0500

    Task Done!

---
+++ Jan-15-2025/Image Slider/script.js
@@ -0,0 +1,23 @@
+const slides = document.querySelectorAll(".slide");
+let counter = 0;
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+const slideImage = () => {
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+};
+const goPrev = () => {
+  if (counter != 0) {
+    counter--;
+  }
+  slideImage();
+};
+const goNext = () => {
+  if (counter < slides.length - 1) {
+    counter++;
+  }
+  slideImage();
+};
